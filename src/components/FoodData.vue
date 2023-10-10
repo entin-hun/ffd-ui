f72fda1fefd491ed8df941ee1771cce8ff185b30
Szabolcs Szekelyi <szabi@aura-labs.cc>
2023-10-10 17:53:26 +0200

    Handle and display errors in input data

--- src/components/FoodData.vue
+++ src/components/FoodData.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="column justify-center" v-if="data !== undefined">
+    <q-banner
+      rounded
+      class="bg-warning text-black q-my-md"
+      v-if="dataErrors.length > 0"
+    >
+      <template v-slot:avatar>
+        <q-avatar icon="warning" class="bg-primary text-white" square rounded />
+      </template>
+      <template v-slot:default
+        ><div>
+          Some data sources were unavailable or invalid. Data below are based on
+          partial information:
+          <ul>
+            <li v-for="error in dataErrors" :key="error">{{ error }}</li>
+          </ul>
+        </div></template
+      >
+    </q-banner>
     <NutrientCharts :data="data" />
     <FoodFacts :data="data" />
-    <ProcessMap :data="data" />
-    <FoodChainTree :data="data" />
+    <ProcessMap :data="data" @show-process="showProcess" />
+    <FoodChainTree :data="data" ref="tree" />
   </div>
   <!-- <div
     v-else-if="request.isFetching && !request.isFinished"
@@ -23,9 +41,11 @@ import ProcessMap from 'components/ProcessMap.vue';
 import FoodChainTree from './FoodChainTree.vue';
 import FoodFacts from './FoodFacts.vue';
 
-import { Ref, onMounted, ref } from 'vue';
+import { Ref, computed, onMounted, ref } from 'vue';
 // import { useFetch } from '@vueuse/core';
 import {
+  FetchError,
+  FoodInstance,
   Process,
   // FetchError,
   // Pokedex,
@@ -33,7 +53,7 @@ import {
   SaleProcess,
   example,
 } from '../models';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 defineProps<{
   auth: string;
@@ -42,6 +62,7 @@ defineProps<{
 }>();
 
 const data: Ref<SaleProcess | undefined> = ref(undefined);
+const tree = ref<InstanceType<typeof FoodChainTree>>();
 
 function resolveUrls(process: Process): Promise<void> {
   return Promise.all(
@@ -50,9 +71,8 @@ function resolveUrls(process: Process): Promise<void> {
         ? axios
             .get<ProductInstance>(inputInstance.instance)
             .then((result) => Promise.resolve(result.data))
-            .catch((error) =>
+            .catch((error: AxiosError) =>
               Promise.resolve({
-                responseCode: error.statusCode,
                 errorMessage: error.message,
               })
             )
@@ -75,6 +95,62 @@ function resolveUrls(process: Process): Promise<void> {
 onMounted(() =>
   resolveUrls(example.sale).then(() => (data.value = example.sale))
 );
+
+function showProcess(processId: number) {
+  tree.value?.openProcess(processId);
+}
+
+const dataErrors = computed(() => [
+  ...checkUnresolvedUrls(data.value),
+  ...(data.value === undefined
+    ? []
+    : data.value.inputInstances
+        .map((instance) =>
+          typeof instance.instance === 'object' && 'type' in instance.instance
+            ? checkNutrients(instance.instance)
+            : []
+        )
+        .flat()),
+]);
+
+function checkUnresolvedUrls(process?: Process): string[] {
+  return process === undefined
+    ? []
+    : process.inputInstances
+        .map((inputInstance) =>
+          typeof inputInstance.instance === 'object'
+            ? 'errorMessage' in inputInstance.instance
+              ? [
+                  `${process.type} has unresolvable input instance (${inputInstance.instance.errorMessage})`,
+                ]
+              : 'process' in inputInstance.instance
+              ? checkUnresolvedUrls(inputInstance.instance.process)
+              : []
+            : []
+        )
+        .flat();
+}
+
+function checkNutrients(instance: FoodInstance): string[] {
+  if (instance.nutrients !== undefined || instance.iDs !== undefined) return [];
+
+  if (instance.process === undefined)
+    return [`${instance.type} is missing nutrient information`];
+
+  const inputErrors =
+    instance.process === undefined
+      ? []
+      : instance.process.inputInstances
+          .map((inputInstance) =>
+            typeof inputInstance.instance === 'object' &&
+            'type' in inputInstance.instance
+              ? checkNutrients(inputInstance.instance)
+              : []
+          )
+          .flat();
+
+  return inputErrors;
+}
 
 // const url = `https://nft.api.infura.io/networks/137/nfts/${props.contract}/tokens/${props.token}`;
 
--- src/components/NutrientCharts.vue
+++ src/components/NutrientCharts.vue
@@ -21,7 +21,7 @@
 
 <script setup lang="ts">
 import { Ref, computed, onMounted, ref } from 'vue';
-import { Process, FoodInstance, SaleProcess } from '../models';
+import { Process, FoodInstance, SaleProcess, FetchError } from '../models';
 import FoodDataBanner from './FoodDataBanner.vue';
 import FoodInstanceSelector from './FoodInstanceSelector.vue';
 import {
@@ -37,10 +37,15 @@ const props = defineProps<{
   data: SaleProcess;
 }>();
 
+interface ResolvedFallbackNutrient {
+  amount: number;
+  nutrient: Nutrient;
+}
+
 export interface FoodNutrients {
   instance: FoodInstance;
   color: string;
-  nutrients: FoodNutrient[];
+  nutrients: (FoodNutrient | ResolvedFallbackNutrient)[] | FetchError;
 }
 
 const foodNutrients: Ref<FoodNutrients[]> = ref([]);
@@ -53,7 +58,7 @@ const nutrientsOfInterest = [1079, 1087, 1089, 1090, 1162, 1092, 1095];
 
 let chartColorCounter = 0;
 
-async function findComponentNutrients(
+async function findInstanceNutrients(
   process: Process
 ): Promise<FoodNutrients[]> {
   return Promise.all(
@@ -64,31 +69,48 @@ async function findComponentNutrients(
               {
                 instance: instance,
                 color: chartColors[chartColorCounter++ % chartColors.length],
-                nutrients: await fdcApi
-                  .getFood(
-                    instance.iDs.id.toFixed(),
-                    undefined,
-                    nutrientsOfInterest
-                  )
-                  .then((response) =>
-                    'foodNutrients' in response.data &&
-                    response.data.foodNutrients !== undefined
-                      ? Promise.resolve(
-                          response.data.foodNutrients.filter(
-                            (item): item is FoodNutrient =>
-                              'nutrient' in item &&
-                              item.nutrient !== undefined &&
-                              item.nutrient?.id != undefined &&
-                              nutrientsOfInterest.includes(item.nutrient.id)
-                          )
+                nutrients:
+                  instance.iDs !== undefined
+                    ? await fdcApi
+                        .getFood(
+                          instance.iDs.id.toFixed(),
+                          undefined,
+                          nutrientsOfInterest // doesn't work so we filter ourselves later
                         )
-                      : Promise.reject()
-                  )
-                  .catch(),
+                        .then((response) =>
+                          typeof response.data === 'object' &&
+                          'foodNutrients' in response.data &&
+                          response.data.foodNutrients !== undefined
+                            ? Promise.resolve(
+                                response.data.foodNutrients.filter(
+                                  (item): item is FoodNutrient =>
+                                    'nutrient' in item &&
+                                    item.nutrient !== undefined &&
+                                    item.nutrient?.id != undefined &&
+                                    nutrientsOfInterest.includes(
+                                      item.nutrient.id
+                                    )
+                                )
+                              )
+                            : Promise.reject()
+                        )
+                        .catch((error) =>
+                          Promise.resolve({
+                            errorMessage: error.message,
+                          })
+                        )
+                    : // : instance.nutrients !== undefined
+                      // ?
+                      //   instance.nutrients.map<ResolvedFallbackNutrient>((nutrient) => ({
+                      //     amount: nutrient.amount,
+                      //     nutrient: fdcApi.get
+                      //   }))
+
+                      [],
               },
             ]
           : 'process' in instance && instance.process !== undefined
-          ? findComponentNutrients(instance.process)
+          ? findInstanceNutrients(instance.process)
           : []
         : []
     )
@@ -102,24 +124,26 @@ const allNutrients = computed(() => {
   > = new Map();
 
   foodNutrients.value.forEach((foodNutrients) => {
-    foodNutrients.nutrients.forEach((nutrient) => {
-      if (
-        !('nutrient' in nutrient) ||
-        nutrient.amount === 0 ||
-        nutrient.nutrient === undefined ||
-        nutrient.nutrient.id === undefined
-      )
-        return;
-      const nutrients = nutrientsById.get(nutrient.nutrient.id);
-      if (nutrients === undefined) {
-        nutrientsById.set(nutrient.nutrient.id, {
-          nutrient: nutrient.nutrient,
-          foods: [foodNutrients],
+    'errorMessage' in foodNutrients.nutrients
+      ? []
+      : foodNutrients.nutrients.forEach((nutrient) => {
+          if (
+            !('nutrient' in nutrient) ||
+            nutrient.amount === 0 ||
+            nutrient.nutrient === undefined ||
+            nutrient.nutrient.id === undefined
+          )
+            return;
+          const nutrients = nutrientsById.get(nutrient.nutrient.id);
+          if (nutrients === undefined) {
+            nutrientsById.set(nutrient.nutrient.id, {
+              nutrient: nutrient.nutrient,
+              foods: [foodNutrients],
+            });
+          } else {
+            nutrients.foods.push(foodNutrients);
+          }
         });
-      } else {
-        nutrients.foods.push(foodNutrients);
-      }
-    });
   });
 
   return nutrientsById;
@@ -127,12 +151,12 @@ const allNutrients = computed(() => {
 
 const enabledFoodNutrients = ref(Array<FoodNutrients>());
 
-onMounted(() => {
-  findComponentNutrients(props.data).then((result) => {
+onMounted(() =>
+  findInstanceNutrients(props.data).then((result) => {
     foodNutrients.value = result;
     enabledFoodNutrients.value = foodNutrients.value;
-  });
-});
+  })
+);
 
 function foodsChanged(components: FoodNutrients[]) {
   enabledFoodNutrients.value = components;
