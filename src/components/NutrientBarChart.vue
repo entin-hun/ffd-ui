a5ecd82ea70fa0ff76b74546a02a359f9c4f2123
Szabolcs Szekelyi <szabi@aura-labs.cc>
2023-10-11 22:20:13 +0200

    Implement chart scales

--- src/components/NutrientBarChart.vue
+++ src/components/NutrientBarChart.vue
@@ -5,10 +5,9 @@
     </div>
     <div class="chart-container col-7" style="position: relative; height: 2em">
       <Bar id="chart" :options="chartOptions" :data="chartData" />
-      <!-- Middle column -->
     </div>
-    <div class="col-2 q-px-md">
-      {{ Math.round(enabledAmounts * 100) / 100 }} {{ nutrient.unitName }}
+    <div class="col-2 q-px-md text-no-wrap">
+      {{ Math.round(enabledAmounts * 100) / 100 }} {{ unit }}
     </div>
   </div>
 </template>
@@ -16,7 +15,7 @@
 <script setup lang="ts">
 import { Nutrient } from 'src/fdc_api/generated/api';
 import { computed } from 'vue';
-import { FoodNutrients } from './NutrientCharts.vue';
+import { ChartScale, FoodNutrients, NutrientRdi } from './NutrientCharts.vue';
 import { Bar } from 'vue-chartjs';
 import {
   Chart as ChartJS,
@@ -36,6 +35,9 @@ const props = defineProps<{
   nutrient: Nutrient;
   allFoods: FoodNutrients[];
   enabledFoods: FoodNutrients[];
+  scale: ChartScale;
+  totalWeight: number;
+  rdi: NutrientRdi;
 }>();
 
 const chartData = computed((): ChartData<'bar'> => {
@@ -77,28 +79,32 @@ const enabledAmounts = computed(() =>
 );
 
 function getEnabledAmount(food: FoodNutrients): number {
-  return props.enabledFoods.includes(food)
-    ? (food.nutrients.constructor === Array &&
-        food.nutrients.find(
-          (foodNutrient) =>
-            'nutrient' in foodNutrient &&
-            foodNutrient.nutrient?.id === props.nutrient.id
-        )?.amount) ||
-        0
-    : 0;
+  return mapAmount(
+    props.enabledFoods.includes(food)
+      ? (food.nutrients.constructor === Array &&
+          food.nutrients.find(
+            (foodNutrient) =>
+              'nutrient' in foodNutrient &&
+              foodNutrient.nutrient?.id === props.nutrient.id
+          )?.amount) ||
+          0
+      : 0
+  );
 }
 
 const totalAmounts = computed(() =>
-  props.allFoods
-    .map(
-      (food) =>
-        (food.nutrients.constructor === Array &&
-          food.nutrients.find(
-            (nutrient) => nutrient.nutrient?.id === props.nutrient.id
-          )?.amount) ||
-        0
-    )
-    .reduce((prev, cur) => prev + cur, 0)
+  mapAmount(
+    props.allFoods
+      .map(
+        (food) =>
+          (food.nutrients.constructor === Array &&
+            food.nutrients.find(
+              (nutrient) => nutrient.nutrient?.id === props.nutrient.id
+            )?.amount) ||
+          0
+      )
+      .reduce((prev, cur) => prev + cur, 0)
+  )
 );
 
 const chartOptions = computed(
@@ -111,7 +117,12 @@ const chartOptions = computed(
       x: {
         stacked: true,
         min: 0,
-        max: totalAmounts.value,
+        max:
+          props.scale === 'rdi_child' ||
+          props.scale === 'rdi_female' ||
+          props.scale === 'rdi_male'
+            ? 200
+            : totalAmounts.value,
         ticks: {
           display: false,
         },
@@ -137,5 +148,26 @@ const chartOptions = computed(
       },
     },
   })
+);
+
+function mapAmount(amount: number): number {
+  switch (props.scale) {
+    case 'total':
+      return amount;
+    case 'normalized':
+      return (amount / props.totalWeight) * 100;
+    case 'rdi_child':
+      return (amount / props.rdi.child) * 100;
+    case 'rdi_female':
+      return (amount / props.rdi.female) * 100;
+    case 'rdi_male':
+      return (amount / props.rdi.male) * 100;
+  }
+}
+
+const unit = computed(() =>
+  props.scale === 'total' || props.scale === 'normalized'
+    ? props.nutrient.unitName
+    : '%'
 );
 </script>
--- src/components/NutrientCharts.vue
+++ src/components/NutrientCharts.vue
@@ -1,26 +1,42 @@
 <template>
   <div class="q-pb-lg">
     <FoodDataBanner icon="percent">
-      Nutrients
-      <q-icon name="info">
-        <q-popup-proxy>
-          <q-banner>
-            <div style="max-width: 30em">
-              Actual nutritional values may vary due to natural variations in
-              ingredients and processing. Nutritional values are rounded,
-              according to regulatory guidelines. Your total RDI values may be
-              higher or lower depending on your calorie needs. While we strive
-              to keep the nutritional information accurate, the values provided
-              are estimates and may not reflect all accurate values. Nutritional
-              information was sourced from the
-              <a href="https://fdc.nal.usda.gov/fdc-app.html#/"
-                >United States Department of Agriculture</a
-              >
-              upon loading the site.
-            </div>
-          </q-banner>
-        </q-popup-proxy>
-      </q-icon>
+      <div class="row justify-between items-center">
+        <div class="col">
+          Nutrients
+          <q-icon name="info_outline">
+            <q-popup-proxy>
+              <q-banner>
+                <div style="max-width: 30em">
+                  Actual nutritional values may vary due to natural variations
+                  in ingredients and processing. Nutritional values are rounded,
+                  according to regulatory guidelines. Your total RDI values may
+                  be higher or lower depending on your calorie needs. While we
+                  strive to keep the nutritional information accurate, the
+                  values provided are estimates and may not reflect all accurate
+                  values. Nutritional information was sourced from the
+                  <a
+                    href="https://fdc.nal.usda.gov/fdc-app.html#/"
+                    target="_blank"
+                    >United States Department of Agriculture</a
+                  >
+                  upon loading the site.
+                </div>
+              </q-banner>
+            </q-popup-proxy>
+          </q-icon>
+        </div>
+        <q-select
+          dense
+          :options="chartScaleOptions"
+          v-model="chartScale"
+          style="width: 10em"
+          :map-options="true"
+          emit-value
+          class="q-mr-sm"
+          borderless
+        />
+      </div>
     </FoodDataBanner>
     <FoodInstanceSelector
       :food-nutrients="foodNutrients"
@@ -32,9 +48,16 @@
       class="q-mx-md row justify-between"
     >
       <NutrientBarChart
+        v-if="
+          nutrient.nutrient.id !== undefined &&
+          nutrientsOfInterest.has(nutrient.nutrient.id)
+        "
         :all-foods="foodNutrients"
         :nutrient="nutrient.nutrient"
         :enabled-foods="enabledFoodNutrients"
+        :scale="chartScale"
+        :total-weight="data.inputInstances[0].quantity"
+        :rdi="nutrientsOfInterest.get(nutrient.nutrient.id)!"
       />
     </div>
     <FoodImpacts :enabled-foods="enabledFoodNutrients" />
@@ -55,17 +78,36 @@ import {
 import { chartColors } from './utils';
 import NutrientBarChart from './NutrientBarChart.vue';
 import FoodImpacts from './FoodImpacts.vue';
+import { QSelectOption } from 'quasar';
 
 const props = defineProps<{
   data: SaleProcess;
 }>();
 
+export type ChartScale =
+  | 'total'
+  | 'normalized'
+  | 'rdi_child'
+  | 'rdi_female'
+  | 'rdi_male';
+
+const chartScaleOptions = computed((): QSelectOption<ChartScale>[] => [
+  {
+    value: 'total',
+    label: `Total (${props.data.inputInstances[0].quantity} g)`,
+  },
+  { value: 'normalized', label: 'Per 100 g' },
+  { value: 'rdi_child', label: '% RDI (child)' },
+  { value: 'rdi_female', label: '% RDI (female)' },
+  { value: 'rdi_male', label: '% RDI (male)' },
+]);
+
+const chartScale: Ref<ChartScale> = ref('total');
+
 interface ResolvedFallbackNutrient {
   amount: number;
   nutrient: Nutrient;
 }
-
-const disclaimerVisible = ref(false);
 
 export interface FoodNutrients {
   instance: FoodInstance;
@@ -79,7 +121,28 @@ const fdcApi = new FDCApi(
   new Configuration({ apiKey: 'gQazulH9pUtEzMGwP1ecHI3762sRTAHsWUu7dzmB' })
 );
 
-const nutrientsOfInterest = [1079, 1087, 1089, 1090, 1162, 1092, 1095];
+export interface NutrientRdi {
+  child: number;
+  male: number;
+  female: number;
+}
+
+const nutrientsOfInterest: Map<number, NutrientRdi> = new Map([
+  /* Fiber, total dietary */
+  [1079, { child: 25, female: 12.6, male: 16.7 }],
+  /* Calcium, Ca */
+  [1087, { child: 1, female: 800, male: 1000 }],
+  /* Iron, Fe */
+  [1089, { child: 10, female: 7, male: 10 }],
+  /* Magnesium, Mg */
+  [1090, { child: 130, female: 170, male: 170 }],
+  /* Vitamin C, total ascorbic acid */
+  [1162, { child: 25, female: 75, male: 75 }],
+  /* Potassium, K */
+  [1092, { child: 2, female: 2600, male: 3400 }],
+  /* Zinc, Zn */
+  [1095, { child: 5, female: 8, male: 10 }],
+]);
 
 let chartColorCounter = 0;
 
@@ -100,7 +163,7 @@ async function findInstanceNutrients(
                         .getFood(
                           instance.iDs.id.toFixed(),
                           undefined,
-                          nutrientsOfInterest // doesn't work so we filter ourselves later
+                          Array.from(nutrientsOfInterest.keys()) // doesn't work so we filter ourselves later
                         )
                         .then((response) =>
                           typeof response.data === 'object' &&
@@ -112,9 +175,7 @@ async function findInstanceNutrients(
                                     'nutrient' in item &&
                                     item.nutrient !== undefined &&
                                     item.nutrient?.id != undefined &&
-                                    nutrientsOfInterest.includes(
-                                      item.nutrient.id
-                                    )
+                                    nutrientsOfInterest.has(item.nutrient.id)
                                 )
                               )
                             : Promise.reject()
