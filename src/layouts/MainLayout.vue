572b712d339a3a8be673dc275077c47708af5e10
Szabolcs Szekelyi <szabi@aura-labs.cc>
2023-10-15 06:45:00 +0200

    Add partner link

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,9 +7,26 @@
         </div>
         <router-view />
         <div class="row justify-center bg-primary text-white">
-          <span class="q-pa-sm">Made with 🥦 by FAIRFOODDATA, LDA.</span>
+          <span class="q-pa-sm"
+            ><div class="column items-center">
+              <div>Made with 🥦 by FAIRFOODDATA, LDA.</div>
+              <a href="https://www.fairfooddata.com/" target="_blank"
+                >Partner with us <q-icon name="open_in_new"
+              /></a></div
+          ></span>
         </div>
       </q-page-container>
     </div>
   </q-layout>
 </template>
+
+<style>
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
